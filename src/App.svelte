cc882b8ee4540a68ad269d69b8213f0a6f80fb40
Siddharth Goswami <vistax86live@gmail.com>
2020-12-12 18:01:21 +0530

    Read query params & add manifest

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,21 @@
 <script>
   let name = 'there';
+  let status = '';
+
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(function (reg) {
+        status = 'Service worker registered.';
+      })
+      .catch(function (err) {
+        status = 'Service worker not registered. This happened: ' + err.message;
+      });
+  }
+
+  let queryParams = new URLSearchParams(location.search);
 </script>
 
 <h1>Hello {name}!</h1>
+<div>Query params: ${queryParams}</div>
+<div>Status: {status}</div>
